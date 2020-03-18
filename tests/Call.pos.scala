b3738a06f952be977955bf147bc14ff970812683
Dale Wijnand <dale.wijnand@gmail.com>
2020-03-18 14:19:23 +0000

    Save the empty checkfiles for Call

---
+++ tests/Call.pos.scala
@@ -0,0 +1,86 @@
+class CR extends Runnable { def run() = () }
+class CS { override def toString   = "" }
+class CJ { override def toString() = "" }
+
+case class CCR() extends Runnable { def run() = () }
+case class CCS() { override def toString   = "" }
+case class CCJ() { override def toString() = "" }
+
+class VCR(val x: String) extends AnyVal
+class VCS(val x: String) extends AnyVal { override def toString   = "" }
+class VCJ(val x: String) extends AnyVal { override def toString() = "" }
+
+case class VCCR(x: String) extends AnyVal
+case class VCCS(x: String) extends AnyVal { override def toString   = "" }
+case class VCCJ(x: String) extends AnyVal { override def toString() = "" }
+
+class Test {
+  val any: Any    = ""
+  val ref: AnyRef = ""
+  val obj: Object = ""
+  val str: String = ""
+
+  any.##
+  any.getClass
+  any.getClass()
+  any.hashCode
+  any.hashCode()
+  any.toString
+  any.toString()
+
+  ref.##
+  ref.getClass
+  ref.getClass()
+  ref.hashCode
+  ref.hashCode()
+  ref.toString
+  ref.toString()
+
+  obj.##
+  obj.getClass
+  obj.getClass()
+  obj.hashCode
+  obj.hashCode()
+  obj.toString
+  obj.toString()
+
+  str.##
+  str.getClass
+  str.getClass()
+  str.hashCode
+  str.hashCode()
+  str.toString
+  str.toString()
+
+  new CR().run
+  new CR().run()
+  new CR().toString
+  new CR().toString()
+  new CS().toString
+  new CS().toString()
+  new CJ().toString
+  new CJ().toString()
+
+  new VCR("").toString
+  new VCR("").toString()
+  new VCS("").toString
+  new VCS("").toString()
+  new VCJ("").toString
+  new VCJ("").toString()
+
+  CCR().run
+  CCR().run()
+  CCR().toString
+  CCR().toString()
+  CCS().toString
+  CCS().toString()
+  CCJ().toString
+  CCJ().toString()
+
+  VCCR("").toString
+  VCCR("").toString()
+  VCCS("").toString
+  VCCS("").toString()
+  VCCJ("").toString
+  VCCJ("").toString()
+}
