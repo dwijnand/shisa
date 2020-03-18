dbf2c2ce927ec9e3557ce431396c19ad76fda6f3
Dale Wijnand <dale.wijnand@gmail.com>
2020-03-18 12:44:49 +0000

    Get 2.13.head once

--- src/main/scala/shisa/Main.scala
+++ src/main/scala/shisa/Main.scala
@@ -7,36 +7,47 @@ import scala.jdk.CollectionConverters._
 import scala.sys.process._
 
 object Main {
-  val scalac2 = "scalac -deprecation"
-  val scalac3 = "dotc -migration -color:never -explain"
+  def main(args: Array[String]): Unit = {
+    val sourceFile = args match {
+      case Array(sourceFile) => Paths.get(sourceFile)
+      case _                 => sys.error(s"expected file, got $args")
+    }
 
-  // More combinations?
-  // -Xlint:eta-sam         The Java-defined target interface for eta-expansion was not annotated @FunctionalInterface.
-  // -Xlint:eta-zero        Usage `f` of parameterless `def f()` resulted in eta-expansion, not empty application `f()`.
-  val combinations = Seq(
-    Invoke("2.13-base", s"$scalac2 -2.13.1"),
-    Invoke("2.13-head", s"$scalac2 -2.13.head"),
-    Invoke("2.13-new",  s"$scalac2 -2.13.head -Xsource:2.14"),
-    Invoke("3.0-old",   s"$scalac3 -language:Scala2Compat"),
-    Invoke("3.0",       s"$scalac3"),
-    Invoke("3.1",       s"$scalac3 -strict"),
-  )
+    val scalac2 = "scalac -deprecation"
+    val scalac3 = "dotc -migration -color:never -explain"
 
-  def main(args: Array[String]): Unit = args match {
-    case Array(sourceFile) => combinations.foreach(run(_, Paths.get(sourceFile)))
-    case _                 => Console.err.println(s"expected file")
+    val _2_13_head = execStr("scala -2.13.head -e println(scala.util.Properties.versionNumberString)") match {
+      case ExecResult(_, 0, Seq(s)) => s
+      case res                      => sys.error(s"Fail: $res")
+    }
+
+    // More combinations?
+    // -Xlint:eta-sam         The Java-defined target interface for eta-expansion was not annotated @FunctionalInterface.
+    // -Xlint:eta-zero        Usage `f` of parameterless `def f()` resulted in eta-expansion, not empty application `f()`.
+    val combinations = Seq(
+      Invoke("2.13-base", s"$scalac2 -2.13.1"),
+      Invoke("2.13-head", s"$scalac2 -${_2_13_head}"),
+      Invoke("2.13-new",  s"$scalac2 -${_2_13_head} -Xsource:2.14"),
+      Invoke("3.0-old",   s"$scalac3 -language:Scala2Compat"),
+      Invoke("3.0",       s"$scalac3"),
+      Invoke("3.1",       s"$scalac3 -strict"),
+    )
+
+    combinations.foreach(run(_, sourceFile))
   }
 
   def run(inv: Invoke, sourceFile: Path) = {
     import inv.{ id, cmd }
     val checkFile = sourceFile.resolveSibling(sourceFile.getFileName.toString.stripSuffix(".scala") + s".$id.check")
-    val res = exec(tokenise(cmd) :+ sourceFile.toString)
+    val res = execStr(s"$cmd $sourceFile")
     println(res)
     import res.{ exitCode, lines }
     Files.write(checkFile, (s"// exitCode: $exitCode" +: lines).asJava)
   }
 
   def tokenise(s: String) = s.split(' ').toSeq
+
+  def execStr(s: String) = exec(tokenise(s))
 
   def exec(argv: Seq[String]): ExecResult = {
     val buff = new ListBuffer[String]
