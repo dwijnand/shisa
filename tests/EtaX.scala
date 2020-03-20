1682f95355f158d8359238bf5fb989a444dc679c
Dale Wijnand <dale.wijnand@gmail.com>
2020-03-20 10:34:48 +0000

    Shrinking EtaX more

--- tests/EtaX.scala
+++ tests/EtaX.scala
@@ -6,7 +6,11 @@
 
 class A { def boom(): Unit }
 
-class EtaExpandZeroArg { // -Xlint:eta-zero -Xlint:eta-sam
+class EtaExpandZeroArg { // 
+  // [1]:               -Xlint:eta-zero -Xlint:eta-sam
+  // [2]: -Xsource:2.14 -Xlint:eta-zero -Xlint:eta-sam
+  // [3]: -Xsource:2.14 -deprecation -Werror
+
   def foo()       = () => ""
   def bar         = ""
   def baz()       = ""
@@ -20,48 +24,37 @@ class EtaExpandZeroArg { // -Xlint:eta-zero -Xlint:eta-sam
   val t1e: Any       = foo _              // ok, explicit eta-expansion requested
   val t1f: Any       = foo() _            // error: _ must follow method
 
-  val t2a: () => Any = bar     // error: no eta-expansion of zero-arglist-methods (nullary methods)
-  val t2b: () => Any = bar()   // error: bar doesn't take arguments, so expanded to bar.apply(), which misses an argument
-  val t2c: () => Any = bar _   // ok
-  val t2d: Any       = bar _   // ok
-  val t2e: Any       = bar() _ // error: not enough arguments for method apply
+  val t2a: () => Any = bar                   // error: no eta-expansion of zero-arglist-methods (nullary methods)
+  val t2b: () => Any = bar()                 // error: bar doesn't take arguments, so expanded to bar.apply(), which misses an argument
+  val t2c: () => Any = bar _                 // ok
+  val t2d: Any       = bar _                 // ok
+  val t2e: Any       = bar() _               // error: not enough arguments for method apply
+  val t2: () => Any  = bar                   // ?/warn/succ: eta-expanded (-Xlint:eta-zero)
+  val t2Sam0S: Sam0S = bar                   // ?/warn/succ: eta-expanded (-Xlint:eta-zero + -Xlint:eta-sam)
+  val t2Sam0J: Sam0J = bar                   // ?/warn/succ: eta-expanded (-Xlint:eta-zero)
+  val t4: Any        = { val t = bar   ; t } // ?/ok/ok: apply
+  val t7: () => Any  = { val t = bar _ ; t } // ?/ok/ok
 
-  val t3a: () => Any = baz     // eta-expansion, but lint warning
-  val t3b: () => Any = baz _   // ok
-  val t3c: Any       = baz _   // ok
-  val t3d: Any       = baz() _ // error: _ must follow method
+  val t3a: () => Any = baz                   // eta-expansion, but lint warning
+  val t3b: () => Any = baz _                 // ok
+  val t3c: Any       = baz _                 // ok
+  val t3d: Any       = baz() _               // error: _ must follow method
+  val t5: Any        = { val t = baz   ; t } // ?/succ/warn: apply, ()-insertion
+  val t8: () => Any  = { val t = baz _ ; t } // ?/ok/ok
 
   val t4a: () => Any = zap     // eta-expansion, but lint warning
   val t4b: () => Any = zap()   // ditto
   val t4c: () => Any = zap _   // ok
   val t4d: () => Any = zap() _ // ok
 
-  val t5a               = zup // error in 2.13, eta-expansion in 2.14
-  val t5Fun: Any => Any = zup // ok
-  val t5Sam1S: Sam1S    = zup // ok, but warning
-  val t5Sam1J: Sam1J    = zup // ok
-}
+  val t5a               = zup                   // error in 2.13, eta-expansion in 2.14
+  val t5Fun: Any => Any = zup                   // ok
+  val t5Sam1S: Sam1S    = zup                   // ok, but warning
+  val t5Sam1J: Sam1J    = zup                   // ok
+  val t6: Any => Any    = { val t = zup   ; t } // ?: eta-expansion in 2.14
+  val t9: Any => Any    = { val t = zup _ ; t } // ?/ok/ok
 
-class EtaExpand214 { // -Xsource:2.14 {-Xlint:eta-zero -Xlint:eta-sam,-deprecation -Werror}
-  def bar         = ""
-  def baz()       = ""
-  def zup(x: Any) = x
+  new A().boom // ?/?/err: apply, ()-insertion
 
-  val t2: () => Any  = bar                  // warn/succ: eta-expanded (-Xlint:eta-zero)
-  val t2Sam0S: Sam0S = bar                  // warn/succ: eta-expanded (-Xlint:eta-zero + -Xlint:eta-sam)
-  val t2Sam0J: Sam0J = bar                  // warn/succ: eta-expanded (-Xlint:eta-zero)
-  val t3: Any => Any = zup                  // ok
-  val t4: Any        = { val t = bar; t }   // ok: apply
-  val t5: Any        = { val t = baz; t }   // succ/warn: apply, ()-insertion
-  val t6: Any => Any = { val t = zup; t }   // eta-expansion in 2.14
-  val t7: () => Any  = { val t = bar _; t } // ok
-  val t8: () => Any  = { val t = baz _; t } // ok
-  val t9: Any => Any = { val t = zup _; t } // ok
-
-  val a = new A
-  a.boom // ?/err: apply, ()-insertion
-
-  import scala.collection.mutable.Map
-  val xs = Map(1 -> "foo")
-  val ys = xs.clone // ok
+  val ys = { val t = scala.collection.mutable.Map(1 -> "foo"); xs.clone } // ?/?/ok
 }
