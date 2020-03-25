ce58dcad3b433c33bc986143c00cc119833a1a7a
Dale Wijnand <dale.wijnand@gmail.com>
2020-03-25 14:42:26 +0000

    Switch to split sources & lots of compiler calls

--- src/main/scala/shisa/Main.scala
+++ src/main/scala/shisa/Main.scala
@@ -1,13 +1,12 @@
 package shisa
 
-import java.io._
 import java.nio.file._
+import java.nio.file.StandardOpenOption._
 
 import scala.collection.mutable.ListBuffer
 import scala.jdk.CollectionConverters._
 import scala.jdk.StreamConverters._
 import scala.sys.process._
-import scala.util.Using
 import scala.util.chaining._
 
 object Main {
@@ -33,33 +32,28 @@ object Main {
       case res                      => sys.error(s"Fail: $res, lines:\n  ${res.lines.mkString("\n  ")}")
     }
 
-    val scalac2 = "scalac"
-    val scalac3 = "dotc"
-    val scala2  = "scala"
-    val scala3  = "dotr -Djava.util.logging.config.file=log.properties"
-    val opts2   = "-deprecation"
-    val opts3   = "-migration -color:never -explain"
+    val scalac2 = "scalac -deprecation"
+    val scalac3 = "dotc -migration -color:never -explain"
 
     // More combinations?
     // -Xlint:eta-sam         The Java-defined target interface for eta-expansion was not annotated @FunctionalInterface.
     // -Xlint:eta-zero        Usage `f` of parameterless `def f()` resulted in eta-expansion, not empty application `f()`.
     // https://github.com/lampepfl/dotty/issues/8571 dotty options
     val combinations = Seq(
-      Invoke.mk("2.13-base", scalac2, scala2, opts2, s"-2.13.1"),
-      Invoke.mk("2.13-head", scalac2, scala2, opts2, s"-${_2_13_head}"),
-      Invoke.mk("2.13-new",  scalac2, scala2, opts2, s"-${_2_13_head} -Xsource:2.14"),
-      Invoke.mk("3.0-old",   scalac3, scala3, opts3, s"-language:Scala2Compat"),
-      Invoke.mk("3.0",       scalac3, scala3, opts3, ""),
-      Invoke.mk("3.1",       scalac3, scala3, opts3, "-strict"),
+      Invoke("2.13-base", s"$scalac2 -2.13.1"),
+      Invoke("2.13-head", s"$scalac2 -${_2_13_head}"),
+      Invoke("2.13-new",  s"$scalac2 -${_2_13_head} -Xsource:2.14"),
+      Invoke("3.0-old",   s"$scalac3 -language:Scala2Compat"),
+      Invoke("3.0",       s"$scalac3"),
+      Invoke("3.1",       s"$scalac3 -strict"),
     )
 
     sourceFiles.foreach { sourceFile =>
       if (sourceFiles.sizeIs > 1) println(s"  Testing $sourceFile")
-      combinations.zipWithIndex.foreach { case (Invoke(id, compile, interpret), idx) =>
-        if (sourceFile.toString.endsWith(".lines.scala"))
-          doInterpret(id, interpret, sourceFile)
-        else
-          doCompile(id, compile, sourceFile)
+      if (sourceFile.toString.endsWith(".lines.scala")) {
+        doCompileLines(sourceFile, combinations)
+      } else {
+        combinations.foreach { case Invoke(id, cmd) => doCompile(id, cmd, sourceFile) }
       }
     }
   }
@@ -73,22 +67,29 @@ object Main {
     Files.write(chk, (s"// exitCode: $exitCode" +: lines).asJava)
   }
 
-  def doInterpret(id: String, cmd: String, sourceFile: Path) = {
+  def doCompileLines(sourceFile: Path, combinations: Seq[Invoke]) = {
     val name = sourceFile.getFileName.toString.stripSuffix(".lines.scala")
     val dir  = Files.createDirectories(sourceFile.resolveSibling(name))
-    val chk  = dir.resolve(s"$name.$id.check")
-    val Regex = """(?s)(.*)class Test \{(.*)}\n""".r
-    val text = Files.readString(sourceFile) match {
-      case Regex(setup, cases) => s"$setup\n$cases"
+    val outD = Files.createDirectories(Paths.get("target").resolve(dir).resolve(name))
+    val re   = """(?s)(.*)class Test \{\n(.*)\n}\n""".r
+    val (setup0, cases) = Files.readString(sourceFile) match { case re(setup, cases) => (setup, cases) }
+    val setup = setup0.linesIterator.map(_.trim).mkString("\n")
+    val input = cases.linesIterator.toList
+    val count = input.size
+    input.iterator.zipWithIndex.filter { case (l, _) => !l.trim.pipe(s => s.isEmpty || s.startsWith("//")) }.foreach { case (line, _idx) =>
+      val body = Array.fill(count)("")
+      body(_idx) = line
+      val idx = if (_idx < 10) s"0${_idx}" else s"${_idx}"
+      val src = outD.resolve(s"$name.$idx.scala")
+      Files.writeString(src, s"${setup}class Test {\n${body.mkString("\n")}\n}\n")
+      combinations.foreach { case Invoke(id, cmd) =>
+        val out = Files.createDirectories(outD.resolve(s"$name.$id.$idx"))
+        val chk = dir.resolve(s"$name.$id.check")
+        val ExecResult(_, exitCode, lines) = execStr(s"$cmd -d $out $src").tap(println)
+        if (_idx == 0) Files.write(chk, Nil.asJava, TRUNCATE_EXISTING)
+        Files.write(chk, (s"// exitCode: $exitCode $line" +: lines).asJava, CREATE, APPEND)
+      }
     }
-    val input = text.linesIterator.map(_.trim).filter(s => s.nonEmpty && !s.startsWith("//")).toList
-    val buff  = new ListBuffer[String]
-    val writeIn = (out: OutputStream) => Using.resource(new PrintWriter(out))(pw => input.foreach(pw.println(_)))
-    val saveLines = BasicIO.processFully(s => buff += normalize(s))
-    val argv = tokenise(s"$cmd")
-    val exit = Process(argv).run(new ProcessIO(writeIn, saveLines, saveLines)).exitValue()
-    val ExecResult(_, exitCode, lines) = ExecResult(argv, exit, buff.toList).tap(println)
-    Files.write(chk, (s"// exitCode: $exitCode" +: lines).asJava)
   }
 
   def tokenise(s: String) = s.split(' ').toSeq
@@ -99,27 +100,9 @@ object Main {
     val exit = Process(argv) ! ProcessLogger(buff += _, buff += _)
     ExecResult(argv, exit, buff.toList)
   }
-
-  val normalize = Function.chain(Seq(
-    stripLambdaClassName(_),
-    stripIdentityHashCode(_),
-  ))
-
-  def stripIdentityHashCode(s: String) =   hashless.replaceAllIn(s, "$1@XXXXXXXX")
-  def stripLambdaClassName(s: String)  = lambdaless.replaceAllIn(s, "<function>")
-
-  val   hashless = "([^ ])@[a-fA-F0-9]+".r
-  val lambdaless = """[$]*Lambda\$\d+/(?:0x[a-f0-9]{16}|\d+)(@[a-fA-F0-9]+)?""".r
 }
 
-final case class Invoke(id: String, compile: String, interpret: String)
-
-object Invoke {
-  def mk(id: String, scalac: String, scala: String, opts1: String, opts2: String) =
-    Invoke(id, mkStr(scalac, opts1, opts2), mkStr(scala, opts1, opts2))
-
-  private def mkStr(xs: String*) = xs.filter(_.nonEmpty).mkString(" ")
-}
+final case class Invoke(id: String, cmd: String)
 
 final case class ExecResult(argv: Seq[String], exitCode: Int, lines: Seq[String]) {
   override def toString = s"${argv.mkString(" ")} => $exitCode"
