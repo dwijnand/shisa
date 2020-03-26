82a22b0cd406c42cce93c13b2bcfaea13bad6309
Dale Wijnand <dale.wijnand@gmail.com>
2020-03-26 13:19:26 +0000

    Fix boom & clone

--- tests/EtaX/EtaX.boom.lines.scala
+++ tests/EtaX/EtaX.boom.lines.scala
@@ -1,4 +1,4 @@
-trait A { def boom(): Unit }
+class A { def boom(): Unit = () }
 
 class Test {
   new A().boom // ?/?/err: apply, ()-insertion
--- tests/EtaX/EtaX.clone.lines.scala
+++ tests/EtaX/EtaX.clone.lines.scala
@@ -1,3 +1,3 @@
 class Test {
-  val ys = { val t = scala.collection.mutable.Map(1 -> "foo"); xs.clone } // ?/?/ok
+  val ys = { val t = scala.collection.mutable.Map(1 -> "foo"); t.clone } // ?/?/ok
 }
