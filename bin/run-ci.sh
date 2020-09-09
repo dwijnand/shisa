015ec036764daee63cee2027d8cf69efeed2a0a7
Dale Wijnand <dale.wijnand@gmail.com>
2020-09-09 20:39:15 +0100

    Drop calling scala-runners

--- bin/run-ci.sh
+++ bin/run-ci.sh
@@ -2,18 +2,5 @@
 
 die() { echo "Aborting: $*"; exit 1; }
 
-curl -fLO https://github.com/github/hub/releases/download/v2.14.2/hub-linux-amd64-2.14.2.tgz || die fail
-tar xfz hub-linux-amd64-2.14.2.tgz        || die fail
-mv hub-linux-amd64-2.14.2/bin/hub bin/hub || die fail
-
-curl -fLo bin/cs https://git.io/coursier-cli-linux && chmod +x bin/cs  || die fail
-
-git clone https://github.com/dwijnand/scala-runners.git scala-runners || die fail
-(cd scala-runners && git checkout github-token || die fail)
-
-PATH=$PATH:$PWD/bin:$PWD/scala-runners
-dotc -version
-scalac -version
-
 sbt 'shisaMain/run'  || die "sbt run failed"
 git diff --exit-code || die "diff in sources detected"
--- src/scalacI/Deps.scala
+++ src/scalacI/Deps.scala
@@ -4,38 +4,24 @@ import coursier._
 
 object Deps {
   val _2_13_base = "2.13.3"
+  val _2_13_head = "2.13.4-bin-965ab82" // scala -2.13.head -version
   val _3_00_base = "0.23.0"
 
-  lazy val lib_2_13_base = fetch(scala2Art("library", _2_13_base))
-  lazy val lib_2_13_head = fetch(scala2Art("library", _2_13_head), scalaEaRepo)
+  lazy val lib_2_13_base = fetch(scala2Art("scala-library", _2_13_base))
+  lazy val lib_2_13_head = fetch(scala2Art("scala-library", _2_13_head), scalaEaRepo)
 
-  lazy val scalac_2_13_base = fetch(scala2Art("compiler", _2_13_base))
-  lazy val scalac_2_13_head = fetch(scala2Art("compiler", _2_13_head), scalaEaRepo)
-  lazy val scalac_3_00_base = fetch(scala3Art("compiler", _3_00_base))
+  lazy val scalac_2_13_base = fetch(scala2Art("scala-compiler", _2_13_base))
+  lazy val scalac_2_13_head = fetch(scala2Art("scala-compiler", _2_13_head), scalaEaRepo)
+  lazy val scalac_3_00_base = fetch(scala3Art("dotty-compiler", _3_00_base))
 
-  lazy val _2_13_head = getVersion() match {
-    case Exec.Result(0, List(s)) => s
-    case _                       => getVersion() match { // if we get extra lines from Coursier, run again
-      case Exec.Result(0, List(s)) => s
-      case Exec.Result(err, lines) => sys.error(s"Fail: $err, lines:${indentedStr(lines)}")
-    }
-  }
+  val scalaEaRepo = MavenRepository("https://scala-ci.typesafe.com/artifactory/scala-integration")
+  val scalaPrRepo = MavenRepository("https://scala-ci.typesafe.com/artifactory/scala-pr-validation-snapshots")
+  val scala2Org   = Organization("org.scala-lang")
+  val scala3Org   = Organization("ch.epfl.lamp")
 
-  def scalaEaRepo = MavenRepository("https://scala-ci.typesafe.com/artifactory/scala-integration")
-  def scalaPrRepo = MavenRepository("https://scala-ci.typesafe.com/artifactory/scala-pr-validation-snapshots")
-  def scala2Org   = Organization("org.scala-lang")
-  def scala3Org   = Organization("ch.epfl.lamp")
-
-  def scala2Art(n: String, v: String) = gav(scala2Org, ModuleName(s"scala-$n"), v)
-  def scala3Art(n: String, v: String) = gav(scala3Org, ModuleName(s"dotty-${n}_${dottyBv(v)}"), v)
-
+  def scala2Art(n: String, v: String) = Dependency(Module(scala2Org, ModuleName(n)), v)
+  def scala3Art(n: String, v: String) = Dependency(Module(scala3Org, ModuleName(s"${n}_${dottyBv(v)}")), v)
   def dottyBv(v: String) = v.split("\\.").take(2).mkString(".")
 
-  def gav(g: Organization, a: ModuleName, v: String) = Dependency(Module(g, a), v)
   def fetch(dep: Dependency, repos: Repository*) = Fetch().addDependencies(dep).addRepositories(repos: _*).run()
-
-  final val dq     = '"'
-  val findVersion  = s"scala.util.Properties.scalaPropOrNone(${dq}maven.version.number$dq).get"
-  def getVersion() = Exec.execStr(s"scala -2.13.head -e 'println($findVersion)'")
-  def indentedStr(xs: Seq[Any]) = xs.iterator.map("\n  " + _).mkString
 }
