099ae4ddbe465857166feacb60c8515eeea10cd2
Dale Wijnand <dale.wijnand@gmail.com>
2020-09-11 10:15:26 +0100

    Merge InvokeCompiler into Main

--- src/main/Main.scala
+++ src/main/Main.scala
@@ -6,74 +6,53 @@ import java.io.{ File, PrintWriter }
 import java.net.URLClassLoader
 import java.nio.file._
 
+import scala.concurrent.duration._
 import scala.jdk.CollectionConverters._
 import scala.jdk.StreamConverters._
 import scala.util.chaining._
 
 object Main {
+  val cwdAbs = Paths.get("").toAbsolutePath
+
+  val dotcCp = BuildInfo.scalac3Dir +: Deps.scalac_3_00_base
+  val dotcCl = new URLClassLoader(dotcCp.map(_.toURI.toURL).toArray, getClass.getClassLoader)
+  val freshCompiler3Cls  = dotcCl.loadClass("shisa.FreshCompiler3")
+  val freshCompiler3Ctor = freshCompiler3Cls.getConstructor(classOf[String], classOf[Array[File]], classOf[String])
+
+  def FreshCompiler3(id: String, cmd: String): Invoke =
+    freshCompiler3Ctor.newInstance(id, Deps.scalac_3_00_base.toArray, cmd).asInstanceOf[Invoke]
+
+  val combinations = Seq[Invoke](
+    FreshCompiler2("2.13-base", Deps.scalac_2_13_base, ""),
+    FreshCompiler2("2.13-head", Deps.scalac_2_13_head, ""),
+    FreshCompiler2("2.13-new",  Deps.scalac_2_13_head, "-Xsource:3"),
+    FreshCompiler3("3.0-old",                          "-source 3.0-migration"),
+    FreshCompiler3("3.0",                              ""), // assumes -source 3.0 is the default
+    FreshCompiler3("3.1-migr",                         "-source 3.1-migration"),
+    FreshCompiler3("3.1",                              "-source 3.1"),
+  )
+
   def main(args: Array[String]): Unit = {
     val sourceFiles = args.toList match {
-      case Nil  => Files.find(Paths.get("tests"), 10, (p, _) => p.toString.endsWith(".scala"))
-        .toScala(List)
-        .sorted
-        .tap(xs => println(s"Files: ${xs.mkString("[", ", ", "]")}"))
-      case argv  => argv.map(Paths.get(_)).map { p =>
-        if (p.isAbsolute) Paths.get("").toAbsolutePath.relativize(p)
-        else p
-      }
+      case Nil =>
+        val fs = Files.find(Paths.get("tests"), /* maxDepth = */ 10, (p, _) => s"$p".endsWith(".scala")).toScala(List).sorted
+        fs.tap(fs => println(s"Files: ${fs.mkString("[", ", ", "]")}"))
+      case xs => xs.map(Paths.get(_)).map(p => if (p.isAbsolute) cwdAbs.relativize(p) else p)
     }
 
     val missing = sourceFiles.filter(!Files.exists(_))
     if (missing.nonEmpty)
       sys.error(s"Missing source files: ${missing.mkString("[", ", ", "]")}")
 
-    val targetTestsDir = Paths.get("target/tests")
-    if (Files.exists(targetTestsDir)) IOUtil.deleteRecursive(targetTestsDir)
+    if (Files.exists(Paths.get("target/tests")))
+      IOUtil.deleteRecursive(Paths.get("target/tests"))
 
     println(s"Combinations:${combinations.map(i => f"\n  ${i.id}%-9s : ${i.cmd}").mkString}")
 
-    sourceFiles.foreach(InvokeCompiler.compileSwitch(_, combinations))
+    sourceFiles.foreach(compile1(_, combinations))
   }
 
-  val mkFreshCompiler3: (String, String) => Invoke = {
-    val scalaJars = Deps.scalac_3_00_base
-    val urls = (BuildInfo.scalac3Dir +: scalaJars).toArray.map(_.toURI.toURL)
-    val cl = new URLClassLoader(urls, getClass.getClassLoader)
-    val cls = cl.loadClass("shisa.FreshCompiler3")
-    val ctor = cls.getConstructor(classOf[String], classOf[Array[File]], classOf[String])
-    (id: String, cmd: String) => ctor.newInstance(id, scalaJars.toArray, cmd).asInstanceOf[Invoke]
-  }
-
-  val combinations = Seq[Invoke](
-    FreshCompiler2("2.13-base", Deps.scalac_2_13_base, ""),
-    FreshCompiler2("2.13-head", Deps.scalac_2_13_head, ""),
-    FreshCompiler2("2.13-new",  Deps.scalac_2_13_head, "-Xsource:3"),
-  mkFreshCompiler3("3.0-old",                          "-source 3.0-migration"),
-  mkFreshCompiler3("3.0",                              ""), // assumes -source 3.0 is the default
-  mkFreshCompiler3("3.1-migr",                         "-source 3.1-migration"),
-  mkFreshCompiler3("3.1",                              "-source 3.1"),
-  )
-}
-
-sealed abstract class CompileFile(val src: Path) {
-  val name          = src.getFileName.toString.stripSuffix(".scala").stripSuffix(".lines")
-  val dir           = IOUtil.createDirs(src.resolveSibling(name))
-  def chkPath: Path
-  lazy val chk      = new PrintWriter(Files.newBufferedWriter(chkPath), true)
-}
-
-final case class CompileFile1(_src: Path, id: String) extends CompileFile(_src) {
-  val chkPath = dir.resolve(s"$name.$id.check")
-}
-
-final case class CompileFileLine(_src: Path, _idx: Int) extends CompileFile(_src) {
-  val idx     = if (_idx < 10) s"0${_idx}" else s"${_idx}"
-  val src2    = IOUtil.createDirs(Paths.get("target").resolve(dir)).resolve(s"$name.$idx.scala")
-  val chkPath = dir.resolve(s"$name.$idx.check")
-}
-
-object InvokeCompiler {
-  def compileSwitch(sourceFile: Path, combinations: Seq[Invoke]) = {
+  def compile1(sourceFile: Path, combinations: Seq[Invoke]) = {
     val residentCompilers = combinations.map(new CachedInvoke(_))
 
     print(f"> Testing $sourceFile%-50s")
@@ -151,3 +130,21 @@ object InvokeCompiler {
   }
 }
 
+sealed abstract class CompileFile(val src: Path) {
+  val name          = src.getFileName.toString.stripSuffix(".scala").stripSuffix(".lines")
+  val dir           = IOUtil.createDirs(src.resolveSibling(name))
+  def chkPath: Path
+  lazy val chk      = new PrintWriter(Files.newBufferedWriter(chkPath), true)
+}
+
+final case class CompileFile1(_src: Path, id: String) extends CompileFile(_src) {
+  val chkPath = dir.resolve(s"$name.$id.check")
+}
+
+final case class CompileFileLine(_src: Path, _idx: Int) extends CompileFile(_src) {
+  val idx     = if (_idx < 10) s"0${_idx}" else s"${_idx}"
+  val src2    = IOUtil.createDirs(Paths.get("target").resolve(dir)).resolve(s"$name.$idx.scala")
+  val chkPath = dir.resolve(s"$name.$idx.check")
+}
+
+case class TimeoutException(duration: Duration) extends RuntimeException
