32e26aec9ddba73902f00a7e621663651c7c8179
Dale Wijnand <dale.wijnand@gmail.com>
2020-11-28 12:02:14 +0000

    Tweak Call

--- src/main/testdata/Call.scala
+++ src/main/testdata/Call.scala
@@ -6,30 +6,26 @@ import scala.meta._
 object Call {
   def tests = List(hashHash, pos).map(_.testFile)
 
-  sealed trait ClassVariant
-  case object     CaseClass extends ClassVariant
-  case object    ValueClass extends ClassVariant
-  case object RunnableClass extends ClassVariant
+  sealed trait ClsOpt
+  case object CaseCls extends ClsOpt; case object ValCls extends ClsOpt; case object RunCls extends ClsOpt
 
-  final case class Cls(variants: List[ClassVariant], suffix: String = "R") {
+  final case class Cls(variants: List[ClsOpt], suffix: String = "R") {
     val name: Type.Name = variants.foldRight(Type.Name(s"C$suffix")) {
-      case (    CaseClass, name) => name.copy("C" + name.value)
-      case (   ValueClass, name) => name.copy("V" + name.value)
-      case (RunnableClass, name) => name
+      case (CaseCls, name) => name.copy("C" + name.value)
+      case ( ValCls, name) => name.copy("V" + name.value)
+      case ( RunCls, name) => name
     }
     val termName: Term.Name = Term.Name(name.value)
 
     val defn: Defn.Class = variants.foldLeft(q"class $name") {
-      case (cls,     CaseClass) => cls.toCaseClass
-      case (cls,    ValueClass) => cls.toValueClass
-      case (cls, RunnableClass) => cls.withRunnable
+      case (cls, CaseCls) => cls.toCaseClass
+      case (cls,  ValCls) => cls.toValueClass
+      case (cls,  RunCls) => cls.withRunnable
     }
 
-    val inst: Term = defn match {
-      case _ if !defn.isCaseClass && !defn.isValueClass => q"new $name()"
-      case _ if  defn.isCaseClass && !defn.isValueClass => q"$termName()"
-      case _ if !defn.isCaseClass &&  defn.isValueClass => q"new $name($ns)"
-      case _ if  defn.isCaseClass &&  defn.isValueClass => q"$termName($ns)"
+    val inst: Term = {
+      val args = if (defn.isValueClass) List(ns) else Nil
+      if (defn.isCaseClass) q"$termName(..$args)" else q"new $name(..$args)"
     }
 
     lazy val copyS = copy(suffix = "S")
@@ -44,10 +40,10 @@ object Call {
   val obj  = Val(q"obj", t"Object")
   val str  = Val(q"str", t"String")
   val vals = List(any, ref, obj, str)
-  val   CR = Cls(List(RunnableClass))
-  val  CCR = Cls(List(RunnableClass, CaseClass))
-  val  VCR = Cls(List(   ValueClass))
-  val VCCR = Cls(List(   ValueClass, CaseClass))
+  val   CR = Cls(List(RunCls))
+  val  CCR = Cls(List(RunCls, CaseCls))
+  val  VCR = Cls(List(ValCls))
+  val VCCR = Cls(List(ValCls, CaseCls))
   val cls1 = List(CR, CCR, VCR, VCCR)
   val clss = cls1.flatMap(cls => List(cls, cls.copyS, cls.copyJ))
 
@@ -59,7 +55,8 @@ object Call {
     val name     = "Call.##"
     val err2     = err(                   "Int does not take parameters")
     val err3     = err("method ## in class Any does not take parameters")
-    val contents = vals.map(v => TestContents(List(v.defn), List(List(q"${v.name}.##()")), multi(err2, err3))).reduce(_ ++ _).toUnit
+    val mkCnts   = (v: Val) => TestContents(List(v.defn), List(List(q"${v.name}.##()")), multi(err2, err3))
+    val contents = vals.map(mkCnts).reduce(_ ++ _).toUnit
   }
 
   object pos extends MkInMemoryTestFile {
