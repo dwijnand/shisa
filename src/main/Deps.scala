890861e9da7d90f77a4e28af9b1187db2faf372b
Dale Wijnand <dale.wijnand@gmail.com>
2020-11-28 09:43:05 +0000

    Update to 3.0.0-M2

--- src/main/Deps.scala
+++ src/main/Deps.scala
@@ -4,18 +4,16 @@ import coursier._
 
 object Deps {
   val _2_13_head = "2.13.4" // scala -2.13.head -version
-  val _3_00_base = "3.0.0-M1"
+  val _3_00_base = "3.0.0-M2"
 
-  lazy val scalac_2_13 = fetch(scala2Art("scala-compiler",  _2_13_head), scalaEaRepo)
+  lazy val scalac_2_13 = fetch(scala2Art("scala-compiler",  _2_13_head))
   lazy val scalac_3_00 = fetch(scala3Art("scala3-compiler", _3_00_base))
 
-  val scalaEaRepo = MavenRepository("https://scala-ci.typesafe.com/artifactory/scala-integration")
-  val scalaPrRepo = MavenRepository("https://scala-ci.typesafe.com/artifactory/scala-pr-validation-snapshots")
-  val scalaOrg    = Organization("org.scala-lang")
+  val scalaEaRepo = mvn"https://scala-ci.typesafe.com/artifactory/scala-integration"
+  val scalaPrRepo = mvn"https://scala-ci.typesafe.com/artifactory/scala-pr-validation-snapshots"
 
-  def scala2Art(n: String, v: String) = Dependency(Module(scalaOrg, ModuleName(n)), v)
-  def scala3Art(n: String, v: String) = Dependency(Module(scalaOrg, ModuleName(s"${n}_$v")), v)
-  def dottyBv(v: String) = v.split("\\.").take(2).mkString(".")
+  def scala2Art(n: String, v: String) = Dependency(Module(org"org.scala-lang", ModuleName(n)), v)
+  def scala3Art(n: String, v: String) = Dependency(Module(org"org.scala-lang", ModuleName(s"${n}_$v")), v)
 
   def fetch(dep: Dependency, repos: Repository*) = Fetch().addDependencies(dep).addRepositories(repos: _*).run()
 }
